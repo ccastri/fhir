6ae1888aa542cc92ddd1a6d399496e67a5bb28b1
ccastri <camilo.castrillon@uao.edu.co>
2023-09-21 20:49:54 -0500

    FHIR server originis all. Oauth2.0 app.html(ready()) and index.html missing (init())

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,10 +1,63 @@
-import React from 'react'
+import {useEffect} from 'react'
+import axios from 'axios'
+// import FHIR from "fhirclient"
+
 
 const Login = () => {
-  return (
-    <div>
-      <h1>Login</h1>
+        const fhirServerBaseUrl = 'http://localhost:8090/fhir'; // Replace with your FHIR server's URL
+const patientId = 'fe9eba1a-50c0-4e9f-8fa1-fbeaa921e980'; // Replace with the ID or identifier of the patient you want to retrieve
+const patientEndpoint = `${fhirServerBaseUrl}/Patient/${patientId}`;
+
+
+// const patientData: fhir4.Patient = {
+//   resourceType: 'Patient',
+//   name: [{ given: ['John'], family: 'Doe' }],
+//   gender: 'male',
+//   birthDate: '1990-01-01',
+//   // Add other patient attributes as needed
+// };
+
+// const myApp = {}
+//! Registrar la app nuevamente con los links y todo
+const storedAuthorization = localStorage.getItem('fhirAuthorization');
+console.log(storedAuthorization)
+// if (storedAuthorization) {
+//   const authorizationResponse = JSON.parse(storedAuthorization);
+
+//   FHIR.oauth2
+//     .ready(authorizationResponse)
+//     .then((client) => {
+//       myApp.smart = client;
+
+//       // Now you can use the FHIR client in your login page
+//     })
+//     .catch((error) => {
+//       console.error('FHIR client initialization error', error);
+//     });
+// }
+useEffect(() => {
+
+    axios
+    .get(patientEndpoint)
+    .then((response  ) => {
+        // Handle the successful response here, which may contain patient data
+        console.log('Patient data retrieved successfully:', response.data);
+    })
+    .catch((error) => {
+        // Handle any errors that occur during the request
+        console.error('Error retrieving patient data:', error);
+    });
+    
+}, [patientEndpoint])
+    return (
+        <>
+    
+         
+        <div className="w-auto p-8">
+      <h1 className='w-auto'>Login</h1>
     </div>
+        
+        </>   
   )
 }
 
--- src/pages/launch/Launch.tsx
+++ src/pages/launch/Launch.tsx
@@ -1,10 +1,58 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import FHIR from "fhirclient"
 
-const Launch = () => {
+// export type LaunchProps={
+//   onLaunchClick : ()=> void
+// }
+
+
+
+function Launch() {
+
+
+  // Función para manejar la autorización OAuth2
+  const handleAuthorization = () => {
+    // Objeto de configuración para la autorización
+    const authorizationConfig = {
+      clientId: 'myClientId',
+      scope: 'patient/*.read',
+      redirect_uri: '/auth/login',
+      iss: 'http://localhost:8090/fhir'
+      // Otros parámetros de configuración OAuth2 si es necesario
+    };
+
+    // Llamada a la función FHIR.oauth2.authorize con la configuración
+   FHIR.oauth2
+    .authorize(authorizationConfig)
+    .then((response) => {
+      // Store the authorization response in local storage
+      localStorage.setItem('fhirAuthorization', JSON.stringify(response));
+
+      // Redirect to the login page
+      // window.location.href = '/auth/login';
+    })
+      .catch((error) => {
+        // Maneja cualquier error de autorización aquí
+        console.error('Authorization error', error);
+
+        // Puedes mostrar un mensaje de error o realizar otras acciones
+      });
+  };
   return (
-    <div className="bg-red-500 w-full">
-      <h1>Launch goes here bro</h1>
+    <div className= "flex flex-col h-auto w-auto items-center  my-auto justify-center">
+      {/* Your Launch component content */}
+      <div className="border-2 p-8 flex flex-col items-center">
+
+      <h2>Launch Component</h2>
+      <Link
+      className='border-2 group w-auto '
+      to={'/auth/login'} 
+      onClick={handleAuthorization}
+      ><p className='transition-all undeline hover:text-red-500 text-slate-700 transform group-hover:scale-105 border-2 border-green-500   ease-in-out' >Al hacer click acepta los terminos de uso y tratamiento de datos de la app</p></Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Launch
+export default Launch;
